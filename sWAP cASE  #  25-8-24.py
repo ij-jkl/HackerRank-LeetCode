1c68fe103a5196c3a6d11668b6e7e2fe1332456f
Isaac <isaaz.jordan02@gmail.com>
2024-08-25 00:34:15 -0300

    Swap Case Finished

--- sWAP cASE  #  25-8-24.py
+++ sWAP cASE  #  25-8-24.py
@@ -1,2 +1,21 @@
 """ https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true """
 
+def swap_case(s):
+    # List
+    result = []
+    
+    # Looping through the entire word
+    for i in s:
+        #If its a lower letter we make it an Upper case, else its upper word to Lower case
+        if(i.islower()):
+            result.append(i.upper())
+        else:
+            result.append(i.lower())
+            
+     #Needed for good output       
+    return "".join(result)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
