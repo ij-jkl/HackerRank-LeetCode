30300e46da85fa18eef6d4388da2a80a4cd94cd6
Isaac <isaaz.jordan02@gmail.com>
2024-08-27 20:24:29 -0300

    Improving Comments

--- Text Wrap  #  27-8-24.py
+++ Text Wrap  #  27-8-24.py
@@ -4,14 +4,13 @@
 import textwrap
 
 def wrap(string, max_width):
-    # L and X are auxiliar variables, im going to be using them to keep track and adding them with the max width value
+    # L is an auxiliar variable, im going to be using it to keep track 
     l = 0
     
-
     result = []
     
     # We are doing this to keep iterating only the lenght of the string divided by the width of the words
-    # Im dividing with // so i avoid floating numbers, the plus 1 its needed so it runs in all cases
+    # Im dividing with // so I avoid floating numbers, the plus 1 its needed so it runs in all cases
     aux = len(string) // max_width + 1
     for _ in range(aux):
         #We append the strings from L to X, they have the lenght of the max width
