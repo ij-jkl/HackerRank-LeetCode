28a35f35957b32c37df5691b15f5f34ae4529de3
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-03-01 14:08:44 -0300

    feat: Submit Collections.OrderedDict solution with 100% test pass rate

---
+++ Collections.OrderDict #  01-03-2025.py
@@ -0,0 +1,51 @@
+﻿"""https://www.hackerrank.com/challenges/py-collections-ordereddict/problem?isFullScreen=true"""
+
+# Solution passes 100% Test cases.
+
+from collections import OrderedDict
+
+# Number of items
+n = int(input())
+
+# Empty order direct to store items and prices.
+ordered_dict = OrderedDict()
+
+for _ in range(n):
+    # Split the text and price, first we get the item name and then the price
+    *item_name, price = input().split()
+    item_name = " ".join(item_name)
+    price = int(price)
+
+    # If an item is already in the dictionarie we update the price, if not we add it
+    if item_name in ordered_dict:
+        ordered_dict[item_name] += price
+    else:
+        ordered_dict[item_name] = price
+
+# Print and Submit exercise
+for item_name, net_price in ordered_dict.items():
+    print(item_name, net_price)
+
+"""
+Sample Input
+
+9
+BANANA FRIES 12
+POTATO CHIPS 30
+APPLE JUICE 10
+CANDY 5
+APPLE JUICE 10
+CANDY 5
+CANDY 5
+CANDY 5
+POTATO CHIPS 30
+"""
+
+""" 
+Sample Output
+
+BANANA FRIES 12
+POTATO CHIPS 60
+APPLE JUICE 20
+CANDY 20
+"""
