8fd5f9bab9331c47e740caac4800aff68c1f0cad
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-04-14 14:54:27 -0300

    feat: ContainsDuplicate solution working

---
+++ Leet-ContainsDuplicate.py
@@ -0,0 +1,18 @@
+﻿""" https://leetcode.com/problems/contains-duplicate/ """
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+"""Run Time 51ms Beats 5.02%, Memory beats 97.40% """
+
+"""
+Input : nums = [1,2,3,1] - Output : True
+Input : nums = [1,2,3,4] = Output : False
+"""
